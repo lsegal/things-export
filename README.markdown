8612d7aa6e3a2835ed7fb231e97ef0d2958d9824
Loren Segal <lsegal@soen.ca>
2008-06-22 03:33:13 -0400

    Initial import

---
+++ README.markdown
@@ -0,0 +1,40 @@
+Things Export 1.0
+=================
+
+Written by **Loren Segal** in 2008. Licensed under MIT license.
+
+
+SYNOPSIS
+--------
+
+Exports your **Things.app** todo items to a simple webpage using `scp`. It
+is recommended that you add your public key to your remote SSH server for
+this to work smoothly.
+
+
+INSTALL
+-------
+
+`things-export` requires the environment variable `THINGS_REMOTE_PATH` to be
+set to the public www directory that the page will be copied to. You can set
+this manually per session or in your `~/.profile` file with:
+
+    export THINGS_REMOTE_PATH=user@example.com:/var/www/htdocs/todo/
+    
+*Note the username and host that need to be changed.*
+
+You can optionally specify the `THINGS_AUTHOR` environment variable which will
+put your name in the title, ex. "Loren Segal's ToDo List". Defaults to "My ToDo List".
+
+Finally, type `ruby things-export.rb --install` after configuring your environment 
+variables to copy over the necessary support files to the remote server. This will
+also export your database for the first time. After the support files are copied,
+you no longer need to run the script with `--install`.
+
+
+RUNNING
+-------
+
+After things-exporter is installed on the remote server, simply use `ruby things-exporter.rb`
+to update the remote index file. Ideally, you can run this from a cron-job to update your
+Things todo list every now and then.
---
+++ things-export.rb
@@ -0,0 +1,129 @@
+require 'rubygems'
+require 'hpricot'
+require 'markaby'
+require 'ostruct'
+
+if ARGV.index('-v') || ARGV.index('--version')
+  puts "things-export 1.0"
+  exit
+end
+
+if !ENV['THINGS_REMOTE_PATH']
+  STDERR.puts "You need to set THINGS_REMOTE_PATH as the URL to copy the index file with scp."
+  STDERR.puts "Example: username@example.com:/var/www/htdocs/todo/"
+  exit
+end
+
+author = ENV['THINGS_AUTHOR'] ? "#{ENV['THINGS_AUTHOR']}'s" : "My"
+things_remote = ENV['THINGS_REMOTE_PATH']
+
+if ARGV.index('--configure') || ARGV.index('-c') || ARGV.index('--install') || ARGV.index('-i')
+  pub = File.join(File.dirname(__FILE__), 'public')
+  puts "Installing things-export to #{things_remote}"
+  system("scp -r #{pub} #{things_remote}")
+end
+
+todos = {:done => [], :notdone => []}
+$tags = {}
+
+filename = File.expand_path('~/Library/Application Support/Cultured Code/Things/Database.xml')
+doc = Hpricot(File.read(filename))
+
+# Get all tags
+doc.search('object[@type="TAG"]').each do |tag|
+  $tags[tag.attributes["id"]] = tag.search('attribute[@name="title"]').text
+end
+
+# Loop through todo items
+doc.search('object[@type="TODO"]').each do |todo|
+  next if todo.search('relationship[@name="parent"][@idrefs]').empty?
+  which = todo.search('attribute[@name="status"]').text.to_i == 3 ? :done : :notdone
+  
+  obj = OpenStruct.new
+  obj.tags = []
+  if which == :done
+    obj.completed = todo.search('attribute[@name="datecompleted"]').text.to_f
+  elsif t=todo.search('attribute[@name="datedue"]').first
+    obj.duedate = t.inner_html.to_f
+  end
+  
+  if t=todo.search('relationship[@name="tags"][@idrefs]').first
+    obj.tags += t.attributes["idrefs"].split(/\s+/)
+  end
+  
+  obj.title = todo.search('attribute[@name="title"]').text
+  obj.index = todo.search('attribute[@name="index"]').text
+  
+  todos[which] << obj
+end
+todos[:done] = todos[:done].sort_by {|x| x.index }
+todos[:notdone] = todos[:notdone].sort_by {|x| x.index }
+
+def taglist(mab, taglist)
+  taglist.each do |tag|
+    mab.span(:class => 'tag') do
+      span(:class => 'leftcap')
+      span(:class => 'tagtitle') { $tags[tag] }
+      span(:class => 'rightcap')
+    end
+  end
+end
+
+mab = Markaby::Builder.new
+mab.html do
+  head do 
+    title "#{author} ToDo List" 
+    link :rel => 'stylesheet', :href => 'style.css', :type => 'text/css', :charset => 'utf-8'
+  end
+  body do
+    h1 "#{author} Things List"
+
+    ul(:id => 'notdone') do
+      row = 0
+      todos[:notdone].each do |todo|
+        row = !row
+        li(:class => (row ? 'even' : 'odd')) do
+          span(:class => 'leftcap')
+          span(:class => 'title') { todo.title }
+          span(:class => 'rightcap')
+          span(:class => 'duedate') do 
+            (todo.duedate / 86400000).to_i.to_s + " days left"
+          end if todo.duedate 
+          span(:class => 'tags') { taglist(mab, todo.tags) }
+        end
+      end
+    end
+
+    ul(:id => 'done') do
+      row = 0
+      todos[:done].each do |todo|
+        row = !row
+        li(:class => (row ? 'even' : 'odd')) do
+          span(:class => 'leftcap')
+          span(:class => 'completed') { Time.at(todo.completed).strftime("%b %d, %Y") }
+          span(:class => 'title') { todo.title }
+          span(:class => 'rightcap')
+          span(:class => 'tags') { taglist(mab, todo.tags) }
+        end
+      end
+    end
+    
+  end
+end
+
+index = File.join(File.dirname(__FILE__), 'public', 'index.html')
+contents = <<-eof
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+#{mab}
+eof
+
+if !ARGV.index('-f') && File.file?(index) && File.read(index) == contents
+  puts "Todo list is identical to current local copy, no remote update necessary. Use -f to force."
+  exit
+end
+
+File.open(index, "w") {|f| f.write(contents) }
+
+puts "Pushing new list to #{things_remote}"
+system("scp #{index} #{things_remote}")
